09f8cd12f7958e37196df6f2aaa3a941de949bcc
alex01at <alex01at@gmail.com>
2024-03-18 23:33:36 +0100

    Update config.php
    delete informations

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,6 @@
           ini_set('display_errors', 1);
           ini_set('display_startup_errors', 0);
           @define('DB_HOST', 'localhost');
-          @define('DB_USER', 'gigtodo');
-          @define('DB_PASS', 'U$bekistan01');
-          @define('DB_NAME', 'gig');
+          @define('DB_USER', '');
+          @define('DB_PASS', '');
+          @define('DB_NAME', '');
