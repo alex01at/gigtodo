d1e027e7e6ce1935333d16be66673cd2356d18d9
Alexander Prossliner <alex01at@gmail.com>
2024-03-22 08:18:26 +0100

    clean up

--- freelancers.php
+++ freelancers.php
@@ -11,16 +11,13 @@ require_once("functions/functions.php");
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="author" content="<?= $site_author; ?>">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100" rel="stylesheet">
-  <link href="styles/bootstrap.css" rel="stylesheet">
+    <link href="styles/bootstrap.css" rel="stylesheet">
   <link href="styles/custom.css" rel="stylesheet"> <!-- Custom css code from modified in admin panel --->
   <link href="styles/styles.css" rel="stylesheet">
   <link href="styles/freelancers.css" rel="stylesheet">
   <link href="styles/categories_nav_styles.css" rel="stylesheet">
   <link href="font_awesome/css/font-awesome.css" rel="stylesheet">
   <link href="styles/sweat_alert.css" rel="stylesheet">
-  <!-- Optional: include a polyfill for ES6 Promises for IE11 and Android browser -->
-  <script src="js/ie.js"></script>
   <script type="text/javascript" src="js/sweat_alert.js"></script>
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <?php if(!empty($site_favicon)){ ?>
